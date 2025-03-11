0df299ae45ba6f590f46950913508c0441161a68
DarttGoblin <yassine.bazgour@gmail.com>
2025-03-11 00:48:58 +0000

    Major update

--- Dermalyze.js
+++ Dermalyze.js
@@ -1,11 +1,32 @@
 const file_input = document.querySelector('.file-input');
 const take_image = document.querySelector('.take-image');
-const result = document.querySelector('.result');
+const gender_select = document.querySelector('.gender-select');
+const age_select = document.querySelector('.age-select');
+const region_select = document.querySelector('.region-select');
+const result_container = document.querySelector('.result-container');
 
+const age_range = [10, 90];
+const gender_option = ['Male', 'Female'];
+const region_option = ['Head/Neck', 'Upper Limb', 'Lower Limb', 'Torso', 'Palm/Sole', 'Nail', 'Genital Area', 'Unknown/Other'];
 
+GenerateOptions();
+UploadImage();
+TakeImage();
 
 function UploadImage() {
-
+    file_input.onchange = function(event) {
+        let file = event.target.files[0];
+        if (file) {
+            console.log("File Name:", file.name);
+            console.log("File Type:", file.type);
+            console.log("File Size:", file.size, "bytes");
+    
+            let reader = new FileReader();
+            reader.onload = () => console.log("File Content (Base64):", reader.result);
+            reader.readAsDataURL(file);
+        }
+    };
+    
 }
 
 function TakeImage() {
@@ -16,8 +37,8 @@ function TakeImage() {
     cameraInput.capture = 'environment';
     cameraInput.style.display = 'none';
 
-    take_image.addEventListener('click', () => cameraInput.click());
-    cameraInput.addEventListener('change', (event) => {
+    take_image.onclick = function() {cameraInput.click();}
+    cameraInput.onchange = function(event) {
         const file = event.target.files[0];
         if (file) {
             const reader = new FileReader();
@@ -30,7 +51,7 @@ function TakeImage() {
             };
             reader.readAsDataURL(file);
         }
-    });
+    };
 }
 
 function isMobile() {
@@ -40,4 +61,30 @@ function isMobile() {
 if (!isMobile()) {
     take_image.classList.add('hide-take-image');
     take_image.disabled = true;
+}
+
+function GenerateOptions() {
+    // Age Options
+    for (i = age_range[0]; i < age_range[1] + 1; i++) {
+        const option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        age_select.appendChild(option);
+    }
+
+    // Gender Options
+    gender_option.forEach(gender => {
+        const option = document.createElement('option');
+        option.value = gender;
+        option.textContent = gender;
+        gender_select.appendChild(option);
+    });
+
+    // Region Options
+    region_option.forEach(region => {
+        const option = document.createElement('option');
+        option.value = region;
+        option.textContent = region;
+        region_select.appendChild(option);
+    });
 }
