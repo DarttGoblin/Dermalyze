05b75fab5944693504469d697f82c0c88a3e3edf
DarttGoblin <yassine.bazgour@gmail.com>
2025-03-11 22:22:55 +0000

    Major updates

--- Dermalyze.js
+++ Dermalyze.js
@@ -1,5 +1,6 @@
 const file_input = document.querySelector('.file-input');
 const take_image = document.querySelector('.take-image');
+const analyse_skin = document.querySelector('.analyse-skin');
 const gender_select = document.querySelector('.gender-select');
 const age_select = document.querySelector('.age-select');
 const region_select = document.querySelector('.region-select');
@@ -14,22 +15,52 @@ GenerateOptions();
 UploadImage();
 TakeImage();
 
+analyse_skin.onclick = function() {
+    if (CheckValidInput()) {
+        SendToModel();
+    } else {
+        alert('Something is missing...');
+    }
+}
+
+function SendToModel() {
+    fetch('http://localhost:3000', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json',},
+        body: JSON.stringify({ text }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            console.log()
+        }
+
+        else {
+            console.error('Error:', error);
+            alert('There was an error processing your text! Please try again.');
+        }
+    })
+    .catch(err => {
+        console.error('Error:', err);
+        alert('There was an error with the server! Please try again.');
+    });
+}
+
 function UploadImage() {
     file_input.onchange = function(event) {
         let file = event.target.files[0];
         if (file) {
+            if (!file || !file.type.startsWith("image/")) {return;}
+
+            let reader = new FileReader();
             console.log("File Name:", file.name);
             console.log("File Type:", file.type);
-            console.log("File Size:", file.size, "bytes");
-    
-            let reader = new FileReader();
             reader.onload = function(e) {
                 preview.src = e.target.result;
             };
             reader.readAsDataURL(file);
         }
     };
-    
 }
 
 function TakeImage() {
@@ -86,4 +117,15 @@ function GenerateOptions() {
         option.textContent = region;
         region_select.appendChild(option);
     });
+}
+
+function CheckValidInput() {
+    if (age_select.value != '' && 
+        gender_select.value != '' && 
+        region_select.value != '' &&
+        preview.src != ''
+    ) {
+        return true;
+    }
+    else {return false;}
 }
