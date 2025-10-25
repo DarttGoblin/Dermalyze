c796aa7d764d5c92cad06350c38a6da642b26184
DarttGoblin <yassine.bazgour@gmail.com>
2025-10-25 22:23:21 +0100

    Added the report button

--- Dermalyze.Scripts/Home.js
+++ Dermalyze.Scripts/Home.js
@@ -1,5 +1,6 @@
 const analyse = document.querySelector('.analyse');
 const github = document.querySelector('.github');
+const report = document.querySelector('.report');
 const demo = document.querySelector('.demo');
 
 analyse.onclick = function() {
@@ -8,6 +9,11 @@ analyse.onclick = function() {
 
 github.onclick = function() {
     window.open('https://github.com/DarttGoblin/Dermalyze_server', '_blank');
+}
+
+report.onclick = function() {
+    alert('Report will be available soon...');
+    // window.open('', '_blank');
 }
 
 demo.onclick = function() {
